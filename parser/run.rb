54f27b04aff4fea08aa89c85c9b5bcdf9a900d7b
Dana Simmons <dana@simmons-audio.com>
2019-02-08 14:32:34 -0500

    fixed typo in css selector class .word-header

--- parser/run.rb
+++ parser/run.rb
@@ -3,5 +3,6 @@ require_relative "./env.rb"
 
 page = Nokogiri::HTML(RestClient.get("https://www.merriam-webster.com/word-of-the-day"))
 
-page.css("word-header").css("h1")
+header = page.css(".word-header").css("h1")
+
 Pry.start
