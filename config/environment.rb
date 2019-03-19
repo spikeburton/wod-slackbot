b61dfb99dd3b4230a550cfd36a3b377b4a7757e2
spikeburton <ralphburton3@gmail.com>
2019-03-19 00:16:52 -0400

    re-define url

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,8 @@ Bundler.require
 require_relative '../lib/parser'
 require_relative '../lib/bot'
 
-## Turn on logging
-SlackRubyBot::Client.logger.level = Logger::WARN
-
 ## Define the URL to scrape
 WOD_URL = "https://www.merriam-webster.com/word-of-the-day"
+
+## Turn on logging
+SlackRubyBot::Client.logger.level = Logger::WARN
