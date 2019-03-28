8179165749984afc3a1f41a594722766bc181e04
spikeburton <ralphburton3@gmail.com>
2019-03-27 22:03:31 -0400

    add Default class

--- lib/bot.rb
+++ lib/bot.rb
@@ -39,4 +39,12 @@ module WODBot
       client.say(channel: data. channel, text: "🙏")
     end
   end
+
+  class Default < SlackRubyBot::Commands::Base
+    match /^(?<bot>\w*)$/
+
+    def self.call(client, data, _match)
+      client.say(channel: data.channel, text: "Uhhh")
+    end
+  end
 end
