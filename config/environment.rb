cb6ef9cc054f7f05d89ee74275a559ef7395cdba
spikeburton <ralphburton3@gmail.com>
2019-06-25 21:46:57 -0400

    add web.rb to requirements

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ Bundler.require
 
 require_relative '../lib/parser'
 require_relative '../lib/bot'
+require_relative '../lib/web'
 
 ## Define the URL to scrape
 WOD_URL = "https://www.merriam-webster.com/word-of-the-day"
